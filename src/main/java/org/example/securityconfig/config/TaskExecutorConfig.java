a1309497216346b94b869c05fd47cca7409de6c8
Casper <asilbek2001.05@gmail.com>
2024-08-31 20:49:55 +0500

    Change Task executor as basic

--- src/main/java/org/example/securityconfig/config/TaskExecutorConfig.java
+++ src/main/java/org/example/securityconfig/config/TaskExecutorConfig.java
@@ -3,12 +3,16 @@ package org.example.securityconfig.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.util.concurrent.Executor;
+
 @Configuration
-public class TaskExecutorConfig {
-    @Bean
-    public TaskExecutor taskExecutor(){
+public class TaskExecutorConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor=new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(5);
         executor.setMaxPoolSize(10);
