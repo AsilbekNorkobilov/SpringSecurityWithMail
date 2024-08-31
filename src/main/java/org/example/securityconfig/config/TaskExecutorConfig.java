32aa24b025d9c2d419289376bad5555209043898
Casper <asilbek2001.05@gmail.com>
2024-08-31 20:50:12 +0500

    Change Task executor as basic

--- src/main/java/org/example/securityconfig/config/TaskExecutorConfig.java
+++ src/main/java/org/example/securityconfig/config/TaskExecutorConfig.java
@@ -1,8 +1,6 @@
 package org.example.securityconfig.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
