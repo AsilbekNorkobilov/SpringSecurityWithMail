ac171f0aa8b21786e954cdd1f349ca7af401c5b3
Casper <asilbek2001.05@gmail.com>
2024-08-31 20:44:22 +0500

    Task executor configuration

---
+++ src/main/java/org/example/securityconfig/config/TaskExecutorConfig.java
@@ -0,0 +1,20 @@
+package org.example.securityconfig.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class TaskExecutorConfig {
+    @Bean
+    public TaskExecutor taskExecutor(){
+        ThreadPoolTaskExecutor executor=new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("taskExecutor");
+        executor.initialize();
+        return executor;
+    }
+}
