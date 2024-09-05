708dd006c5b351d0d35dc58633967b83a21070df
Casper <asilbek2001.05@gmail.com>
2024-09-05 19:14:12 +0500

    Added swagger

---
+++ src/main/java/org/example/securityconfig/config/OpenApiConfig.java
@@ -0,0 +1,38 @@
+package org.example.securityconfig.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("New Product")
+                        .description("""
+                                Place for description
+                                """)
+                        .version("1.0").contact(new Contact().name("Asilbek Norkobilov")
+                                .email( "asilbek2001.05@gmail.com").url("https://t.me/crazy_001"))
+                        .license(new License().name("License of API")
+                                .url("API license URL")));
+    }
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+}
