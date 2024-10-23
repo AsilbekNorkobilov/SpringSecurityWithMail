5c856b16537b9b3975543f31aef1a4c868904665
Casper <asilbek2001.05@gmail.com>
2024-10-23 15:55:03 +0500

    Fixing task executor

--- src/main/java/org/example/securityconfig/config/TaskExecutorConfig.java
+++ src/main/java/org/example/securityconfig/config/TaskExecutorConfig.java
@@ -1,16 +1,18 @@
 package org.example.securityconfig.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
 @Configuration
-public class TaskExecutorConfig implements AsyncConfigurer {
+public class TaskExecutorConfig {
 
-    @Override
-    public Executor getAsyncExecutor() {
+    @Bean
+    public TaskExecutor myTaskExecutor() {
         ThreadPoolTaskExecutor executor=new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(5);
         executor.setMaxPoolSize(10);
