dbc783b3ac5943931eaeebc425b68ada1efee689
Casper <asilbek2001.05@gmail.com>
2024-10-23 15:55:23 +0500

    Fixing task executor

--- src/main/java/org/example/securityconfig/config/TaskExecutorConfig.java
+++ src/main/java/org/example/securityconfig/config/TaskExecutorConfig.java
@@ -3,10 +3,7 @@ package org.example.securityconfig.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
-import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.Executor;
 
 @Configuration
 public class TaskExecutorConfig {
